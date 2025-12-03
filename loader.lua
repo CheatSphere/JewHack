88799c6adc97cc2557fca5824a7349e6481a5af8
Miwowa1337 <miwow@inboxa.de>
2025-12-02 22:31:49 -0600

    update domain

--- loader.lua
+++ loader.lua
@@ -10,7 +10,7 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 
 ---//
 if game.PlaceId == 2534724415 then
-    loadstring(game:HttpGet("https://erlchook.csphere.vip"))()
+    loadstring(game:HttpGet("https://erlchook.loaders.csphere.vip"))()
 else
     game.StarterGui:SetCore("SendNotification",{Title = "ERLChook",Text = "ERLChook doesn't work on other games than ER:LC!",Duration = 10,})
 end
