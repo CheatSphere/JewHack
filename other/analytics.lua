899179263e6946b176f93dc0401bde5f5eedce8a
miwowa1337 <miwow@inboxa.de>
2026-06-28 00:14:35 -0500

    Create analytics.lua

---
+++ other/analytics.lua
@@ -0,0 +1,32 @@
+cloneref = cloneref or function(obj) return obj end
+
+local HttpService = cloneref(game:GetService("HttpService"))
+local UserInputService = cloneref(game:GetService("UserInputService"))
+local MarketplaceService = cloneref(game:GetService("MarketplaceService"))
+
+local request = http_request or request
+local Analytics = {}
+
+function Analytics:Send(Worker, ID, Name)
+    local Success, Name = pcall(function()
+        return MarketplaceService:GetProductInfo(game.PlaceId).Name
+    end)
+
+    pcall(function()
+        request({
+            Url = Worker,
+            Method = "POST",
+            Headers = { ["Content-Type"] = "application/json" },
+            Body = HttpService:JSONEncode({
+                script_id = ID,
+                script_name = Name or tostring(math.random(1000, 9999)),
+                executor = identifyexecutor and identifyexecutor() or "Unknown",
+                game_id = tostring(game.PlaceId),
+                game_name = Success and Name or "Emergency Response: Liberty County",
+                is_mobile = UserInputService:GetPlatform() == Enum.Platform.Android or UserInputService:GetPlatform() == Enum.Platform.IOS or false,
+            }),
+        })
+    end)
+end
+
+return Analytics
